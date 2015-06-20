PCMan/gtk3-nocsd

commit b3c383964c62b8539130b0308c498fc22efd8b71

Switch to a standard Makefile build system
Also set default CFLAGS to -O2 -g.

Makefile
@@ -1,0 +1,17 @@
+PKG_CONFIG ?= pkg-config
+CFLAGS ?= -O2 -g
+CFLAGS += $(shell ${PKG_CONFIG} --cflags gtk+-3.0) -Wall -fPIC
+LDLIBS = -ldl
+
+libdir ?= /usr/lib
+
+all: libgtk3-nocsd.so
+
+clean:
+	rm -f libgtk3-nocsd.so *.o *~
+
+libgtk3-nocsd.so: gtk3-nocsd.o
+	$(CC) -shared $(CFLAGS) $(LDFLAGS) $(LDLIBS) -o $@ $^
+
+install:
+	install -D -m 0644 libgtk3-nocsd.so $(DESTDIR)$(libdir)/libgtk3-nocsd.so

README
@@ -13,13 +13,13 @@ Luckily, with gtk3-nocsd, we still have a way to (partially) turn it off.
 
 How to use:
 
-1. build the code with the script. (you need pkg-config and gtk+3-dev packages)
-> ./build.sh
-After this you'll have a "gtk3-nocsd.so" file.
+1. build the code. (you need pkg-config and gtk+3-dev packages)
+> make
+After this you'll have a "libgtk3-nocsd.so" file.
 
 2. export some environment variables in your .profile or .bashrc.
 > export GTK_CSD=0
-> export LD_PRELOAD=<"full path" of your gtk3-nocsd.so file>
+> export LD_PRELOAD=<"full path" of your libgtk3-nocsd.so file>
 
 3. Re-login to make the environment variables take effect.
 

build.sh
@@ -1,2 +1,0 @@
-#!/bin/sh
-gcc `pkg-config --cflags gtk+-3.0` -Wall -fPIC -shared -ldl -o gtk3-nocsd.so gtk3-nocsd.c

commit 8fb9169fa1b77f14e294e2da39fe930860bcf795

Fix linking of -ldl
b3c383964c62b8539130b0308c498fc22efd8b71 introduced a regression that
the library was not properly linked against -ldl.

Makefile
@@ -11,7 +11,7 @@ clean:
 	rm -f libgtk3-nocsd.so *.o *~
 
 libgtk3-nocsd.so: gtk3-nocsd.o
-	$(CC) -shared $(CFLAGS) $(LDFLAGS) $(LDLIBS) -o $@ $^
+	$(CC) -shared $(CFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
 
 install:
 	install -D -m 0644 libgtk3-nocsd.so $(DESTDIR)$(libdir)/libgtk3-nocsd.so